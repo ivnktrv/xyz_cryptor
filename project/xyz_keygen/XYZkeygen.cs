4443674c07633da2ea211eabd82a7cdbe68ef42a
ivnktrv <ivanlein777@gmail.com>
2024-05-03 18:41:33 +0300

    removed extra 'space' symbols

--- project/xyz_keygen/XYZkeygen.cs
+++ project/xyz_keygen/XYZkeygen.cs
@@ -2,8 +2,8 @@
 
 public class XYZkeygen
 {
-    private const int  CYCLE  = 16;
-    private const int  XYZ_BIN     = 0b11110000111100101111010;
+    private const int  CYCLE    = 16;
+    private const int  XYZ_BIN  = 0b11110000111100101111010;
 
     public void keygen(ref int outKey)
     {
@@ -13,7 +13,7 @@ public class XYZkeygen
         for (int i = 0; i < CYCLE; i++)
         {
             Random r = new Random();
-            k[i] = r.Next(r.Next(1,100), 256) * r.Next(1,10) ^ XYZ_BIN + r.Next(128,r.Next(1024, 4096)) ^ XYZ_BIN;
+            k[i] = r.Next(r.Next(1,100), 256) * r.Next(1,10) ^ XYZ_BIN + r.Next(128, r.Next(1024, 4096)) ^ XYZ_BIN;
             sum += (k[i]*k[i]) ^ XYZ_BIN ^ r.Next(-1,1);
         }
 
